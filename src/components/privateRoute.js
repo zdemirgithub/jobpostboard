f1b9fb3429ade367dd91dd674ca7f7d97fa8d0d0
zdemirgithub <zehrademir.gm@gmail.com>
2024-11-02 14:16:23 +0300

    Create privateRoute.js

---
+++ src/components/privateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function PrivateRoute({ component: Component, ...rest }) {
+    const isAuthenticated = !!localStorage.getItem('token');
+
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isAuthenticated ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to={{
+                        pathname: "/",
+                        state: { from: props.location }
+                    }} />
+                )
+            }
+        />
+    );
+}
+
+export default PrivateRoute;
